91c06342d0e633ad0e1441745ab6f459fcb2a1c9
Владимир Заворотный <zavorotnyv@yandex.ru>
2023-04-06 20:54:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+
+namespace WordCounter
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var enterFilePath = GetEnterFilePath();
+
+			var wordDict = WordCounter.CountWords(enterFilePath);
+
+			var exitPath = enterFilePath.Replace(".txt", "_WordCount.txt");
+
+			WriteWordCount(wordDict, exitPath);
+		}
+		private static void WriteWordCount(Dictionary<string, int> wordDict, string exitPath)
+		{
+			using (var writer = File.CreateText(exitPath))
+			{
+				var maxWordLength = wordDict.Keys.Max(w => w.Length);
+				var indent = 2;
+				foreach (var kvp in wordDict.OrderByDescending(kvp => kvp.Value))
+				{
+					writer.Write(kvp.Key);
+
+					var spaceCount = maxWordLength + indent - kvp.Key.Length;
+
+					writer.Write(new string(' ', spaceCount));
+					writer.WriteLine(kvp.Value);
+				}
+			}
+		}
+		private static string GetEnterFilePath()
+		{
+			string enterFilePath;
+			do
+			{
+				enterFilePath = RequestEnterFilePath();
+
+				if (Path.HasExtension(enterFilePath) == false)
+				{
+					enterFilePath += ".txt";
+				}
+
+				if (IsCorrectFile(enterFilePath))
+				{
+					break;
+				}
+				else
+				{
+					ShowErrorMessage();
+				}
+			} while (true);
+
+			return enterFilePath;
+		}
+		private static string RequestEnterFilePath()
+		{
+				Console.WriteLine("Введите имя txt файла, если он в одной папке с запущенным приложением, или полный путь:");
+				Console.Write("> ");
+
+				var path = Console.ReadLine();
+				Console.WriteLine();
+
+				return path;
+		}
+		private static void ShowErrorMessage()
+		{
+			Console.WriteLine("Неподходящий файл! Должен быть указан существующий txt файл.");
+			Console.WriteLine();
+		}
+		private static bool IsCorrectFile(string path)
+		{
+			if (File.Exists(path) && Path.GetExtension(path) == ".txt")
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ WordCounter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+	internal class WordCounter
+	{
+		private static readonly string _punctuationSymbolsInWord = "'-_";
+		public static Dictionary<string, int> CountWords(string path)
+		{
+			using (var txtReader = File.OpenText(path))
+			{
+				Dictionary<string, int> wordsDict = new();
+				char currentChar;
+				while (true)
+				{
+					do
+					{
+						if (txtReader.Peek() == -1)
+							return wordsDict;
+
+						currentChar = (char)txtReader.Read();
+					} while (IsWordSymbol(currentChar) == false);
+
+					StringBuilder currentWordBuilder = new();
+					do
+					{
+						currentWordBuilder.Append(currentChar);
+
+						if (txtReader.Peek() == -1)
+							break;
+
+						currentChar = (char)txtReader.Read();
+					} while (IsWordSymbol(currentChar));
+
+					string currentWord = currentWordBuilder.ToString().ToLower();
+
+					if (currentWord.All(ch => _punctuationSymbolsInWord.Contains(ch)) == false)
+						wordsDict[currentWord] = wordsDict.TryGetValue(currentWord, out int counter) ? ++counter : 1;
+				}
+			}
+			
+		}
+		private static bool IsWordSymbol(char ch)
+		{
+			return char.IsLetterOrDigit(ch) || _punctuationSymbolsInWord.Contains(ch);
+		}
+	}
+}
